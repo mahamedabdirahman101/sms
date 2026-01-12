f838f788425dd9f070ac3788f211ab6100c1d8bb
Mohamed Abdirahman <mahamedabdirahman413@gmail.com>
2026-01-12 05:42:32 -0800

    second commiting

--- backend/server.js
+++ backend/server.js
@@ -27,5 +27,8 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+console.log("this is testing git");
 console.log("MONGO_URI:", process.env.MONGO_URI);
+
+
+//THAT IS FIRST COMMIT
